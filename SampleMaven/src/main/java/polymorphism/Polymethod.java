57a08654827ff69b9b6f3fd8fe772799d46a80e8
sinu.marina <sinu.marina@DISLAP-056.distvm.com>
2025-03-07 01:48:35 +0530

    push

---
+++ SampleMaven/src/main/java/polymorphism/Polymethod.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class Polymethod {
+
+	public void method(int a, int b)
+	{
+		int sum = a + b;
+		System.out.println("The sum is : " +sum);
+	}
+	public void method1(int a, int b)
+	{
+		int sum = a + b;
+		System.out.println("The sum is : " +sum);
+	}
+	
+}
---
+++ SampleMaven/src/main/java/polymorphism/Polymethod1.java
@@ -0,0 +1,32 @@
+package polymorphism;
+
+public class Polymethod1 extends Polymethod {
+	
+	public void method(int a, int b)
+	{
+		int sub = a - b;
+		System.out.println("The sub is : " +sub);
+		super.method(80, 10);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Polymethod1 poly = new Polymethod1();
+		poly.method(60, 70);
+		poly.method1(50, 50);
+	
+		
+		
+	}
+
+	@Override
+	public void method1(int a, int b) {
+		// TODO Auto-generated method stub
+		int mul = a * b;
+		System.out.println("The mul is : " +mul);
+		super.method1(50, 60);
+	}
+
+}
