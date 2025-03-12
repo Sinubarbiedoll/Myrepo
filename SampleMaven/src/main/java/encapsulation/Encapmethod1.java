6a94222e8bb12f32727a4ee8b7b1743f7ef45ea9
sinu.marina <sinu.marina@DISLAP-056.distvm.com>
2025-03-12 11:43:42 +0530

    done

---
+++ SampleMaven/src/main/java/encapsulation/Encapmethod1.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class Encapmethod1 {
+
+	public static void main(String[] args) {
+		
+		Encapmethod encap = new Encapmethod();
+		encap.setAge(31);
+		encap.setName("SINU");
+		
+		System.out.println("The age anf Name of student is : " +encap.getAge() + " " +encap.getName());
+		// TODO Auto-generated method stub
+
+	}
+
+}
